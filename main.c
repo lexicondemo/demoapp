780fa1cdfb22b70ec71b99a9cb4f68e1d1474787
Jonas Bjork <jonas.bjork@gmail.com>
2024-01-05 11:14:43 +0100

    Clean up of code

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
-// Author: Jonas and Johan
+/**
+ Author     : Jonas
+ Contributor: Johan
+ Date       : 2024-01-05
+*/
 
 int main(void) {
-  //Prints out hello world using standard C syntax.
-  printf("Hello world!\nHello Mars\n");
+
+  // Prints out hello world using standard C syntax.
+  printf("Hello world!\nHello Mars!\n");
+
   return 0;
 }
+
 
